1478f488a984f6281455d5a02642c1f3b07fb4b0
Numan <numaniftikhar300@gmail.com>
2022-08-29 20:31:18 +0500

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-500 h-14 flex-1'>
+      <div className="searchBar">
+        <input className='' type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
