05b4cc6fabcf2e3fb79d4efa129ece13c44ce010
Numan <numaniftikhar300@gmail.com>
2022-08-29 12:28:33 +0500

    add google roboto font

---
+++ src/components/LoginFrom.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+
+const LoginFrom = () => {
+  const [values, setValues] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  return <div>LoginFrom</div>;
+};
+
+export default LoginFrom;
