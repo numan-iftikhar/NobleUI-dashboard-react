d3545050a4db8071d38d1db919d7094c57f6b079
Numan <numaniftikhar300@gmail.com>
2022-08-29 20:30:58 +0500

    change div to a to make it a  link

--- src/components/LinkItem.jsx
+++ src/components/LinkItem.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { links } from "../data/dummy";
 const LinkItem = ({title, icon}) => {
   return (
-    <div className='flex items-center gap-4 px-6 py-1 pt-2 text-sm'>
+    <div>
+    <a href="#" className='flex items-center gap-4 px-6 py-1 pt-2 text-sm'>
         {icon}{title}
+    </a>
     </div>
   )
 }
