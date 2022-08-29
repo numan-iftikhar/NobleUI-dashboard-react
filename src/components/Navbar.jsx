6af50f554a225208f145fe068a1990513b3c95fd
Numan <numaniftikhar300@gmail.com>
2022-08-30 00:15:33 +0500

    fix navbar on top using sticky

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='bg-red-500 h-14 flex-1'>
+    <div className="bg-red-500 px-8 h-14 flex-1 border border-b w-screen sticky top-0">
       <div className="searchBar">
-        <input className='' type="text" />
+        <input className="p-4" type="text" placeholder="Search here..." />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
