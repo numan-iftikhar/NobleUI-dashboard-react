e0b08c3b16a4bf7981b6d01fcfe52ee6dcc121ec
Numan <numaniftikhar300@gmail.com>
2022-08-29 09:11:55 +0500

    Add dummy.js data file

---
+++ src/data/dummy.js
@@ -0,0 +1,84 @@
+export const links = [
+    {
+      title: 'MAIN',
+      links: [
+        {
+          name: 'Dashboard',
+          icon: <FaCube />,
+        },
+      ],
+    },
+    {
+      title: 'WEB APPS',
+      links: [
+        {
+          name: 'Email',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Chat',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Calendar',
+          icon: <FaCube />,
+        },
+      ],
+    },
+  
+    {
+      title: 'COMPONENTS',
+      links: [
+        {
+          name: 'UI Kit',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Advanced UI',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Forms',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Charts',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Table',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Icons',
+          icon: <FaCube />,
+        },
+      ],
+    },
+    {
+      title: 'PAGES',
+      links: [
+        {
+          name: 'Special pages',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Authentication',
+          icon: <FaCube />,
+        },
+        {
+          name: 'Error',
+          icon: <FaCube />,
+        },
+      ],
+    },
+    {
+      title: 'DOCS',
+      links: [
+        {
+          name: 'Documentation',
+          icon: <FaCube />,
+        },
+      ],
+    },
+  ];
