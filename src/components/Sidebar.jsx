977545c233384eba440a0af9bbb3c2ec89c32b6c
Numan <numaniftikhar300@gmail.com>
2022-08-30 15:21:23 +0500

    make scroll bar appear on md:hover

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,12 +4,12 @@ import { FaCube } from "react-icons/fa";
 import LinkItem from "./LinkItem";
 
 const Sidebar = () => {
-  const headingStyle =
+  const sb__headingStyle =
     "flex items-center gap-4 mb-1 px-6 text-xs text-white font-semibold";
   const activeMenu = true;
 
   return (
-    <div className="sidebar bg-slate-900 h-screen overflow-clip">
+    <div className="sidebar bg-slate-900 h-screen md:overflow-hidden md:hover:overflow-auto pb-10">
       {/* header */}
       <div className="flex sticky top-0 justify-between px-6 py-3 border-y border-gray-600  bg-slate-900">
         <p className="brandName font-bold text-2xl text-gray-100">
@@ -23,19 +23,19 @@ const Sidebar = () => {
       {/* Link items */}
       <div className="mt-10 text-gray-400">
         <div className="mb-4">
-          <p className={headingStyle}>MAIN</p>
+          <p className={sb__headingStyle}>MAIN</p>
           <LinkItem title="Dashboard" icon={<FaCube />} />
         </div>
 
         <div className="mb-4">
-          <p className={headingStyle}>WEB APPS</p>
+          <p className={sb__headingStyle}>WEB APPS</p>
           <LinkItem title="Email" icon={<FaCube />} />
           <LinkItem title="Chat" icon={<FaCube />} />
           <LinkItem title="Calender" icon={<FaCube />} />
         </div>
 
         <div className="mb-4">
-          <p className={headingStyle}>COMPONENTS</p>
+          <p className={sb__headingStyle}>COMPONENTS</p>
           <LinkItem title="UI Kit" icon={<FaCube />} />
           <LinkItem title="Advanced UI" icon={<FaCube />} />
           <LinkItem title="Forms" icon={<FaCube />} />
@@ -45,14 +45,14 @@ const Sidebar = () => {
         </div>
 
         <div className="mb-4">
-          <p className={headingStyle}>PAGES</p>
+          <p className={sb__headingStyle}>PAGES</p>
           <LinkItem title="Special pages" icon={<FaCube />} />
           <LinkItem title="Authentication" icon={<FaCube />} />
           <LinkItem title="Error" icon={<FaCube />} />
         </div>
 
         <div className="mb-4">
-          <p className={headingStyle}>DOCS</p>
+          <p className={sb__headingStyle}>DOCS</p>
           <LinkItem title="Documentation" icon={<FaCube />} />
         </div>
       </div>
